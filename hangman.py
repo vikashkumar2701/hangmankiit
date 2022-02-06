bf9ab90b72559887934a55ff9c2771165df90b98
Vikash Kumar <1906645@kiit.ac.in>
2022-02-07 00:53:00 +0530

    function random countries added

--- hangman.py
+++ hangman.py
@@ -1,10 +1,21 @@
 import string,random
 import time
 import speech_recognition as sr
+import random
+
+# function to generate random name from country
+
+def getCountry():
+    # setting here random country names
+
+    countries = ["India", "Iran", "Nepal", "China"]
+    
+    print(random.choice(countries))
 
 
-# setting here random country names
+getCountry()
 
-countries = ["India", "Iran", "Nepal", "China"]
 
-print(countries)
+
+
+
